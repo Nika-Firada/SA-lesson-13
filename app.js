0d30a342c73461e9d3ab8eee733363fba98cf4fb
Nika-Firada <nika.firada@gmail.com>
2021-12-21 17:43:35 +0400

    simple javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+const number = 5; //number
+const justString = 'it is string'; //string
+const justBoolean = true; //boolean
+const justBoolean1 = false; //boolean
+const und = undefined; //undefined
+const justNull = null; //null
+
+// ცვლადები
+
+const personName = 'Nika';
+const personAge = 23;
+const personHobby = 'going to camping';
+console.log(`My name is ${personName} i am ${personAge} year old and my hobbie is ${personHobby}`);
